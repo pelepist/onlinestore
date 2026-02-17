5ba55d5993619cae43ce2586d884282e9746f876
pelepist <petteri.lepisto@hotmail.com>
2026-02-17 11:09:07 +0200

    fixed stuff

--- onlinestore/models/products_model.js
+++ onlinestore/models/products_model.js
@@ -11,18 +11,20 @@ const products={
         return db.query("INSERT INTO products (name,price,saldo) VALUES (?,?,?)",[
             newP.name, newP.price, newP.saldo
         ],callback)
+    },
+
+    update(idP,newP,callback){
+        return db.query("UPDATE products SET name=?, price=?, saldo=? WHERE id_products=?",
+        [
+            newP.name, newP.price, newP.saldo, idP
+        ],callback)
+    },
+    
+    delete(idP,callback){
+        return db.query("DELETE FROM products WHERE id_products=?",[idP],callback)
     }
 }
 
-update(idP,newP,callback){
-    return db.query("UPDATE products SET name=?, price=?, saldo=? WHERE id_products=?",
-    [
-        newP.name, newP.price, newP.saldo, idP
-    ],callback)
-};
-delete(idP,callback){
-    return db.query("DELETE FROM products WHERE id_products=?",[idP],callback)
-};
 
 
 module.exports=products;
